006e8d21422255555a2f394e15fc4c1ded550651
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-05-23 23:11:52 -0300

    Update GoogleAds.tsx

--- src/integrations/GoogleAds.tsx
+++ src/integrations/GoogleAds.tsx
@@ -4,15 +4,18 @@ import React from 'react';
 const GoogleAds: React.FC = () => {
   return (
     <Head>
-      <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17110235811">
-        </script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-17110235811');
-        </script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17110235811">
+      </script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17110235811');
+          `,
+        }}
+      />
     </Head>
   );
 };
