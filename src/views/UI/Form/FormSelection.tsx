fafb12c7aa7514daf3353d7e1e6b864569229555
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-05-27 04:05:50 -0300

    Update FormSelection.tsx

--- src/views/UI/Form/FormSelection.tsx
+++ src/views/UI/Form/FormSelection.tsx
@@ -29,10 +29,10 @@ const FormSelection: React.FC<FormSelectionProps> = ({
                 className={styles.input}
             >
                 <option value="" disabled>Selecione o assunto</option>
-                <option value="site">Criação de site</option>
-                <option value="sistema">Sistema interno</option>
-                <option value="diagnostico">Diagnóstico de maturidade</option>
-                <option value="manutencao">Manutenção e evolução</option>
+                <option value="site">Criação de site corporativo, institucional ou landing page</option>
+                <option value="sistema">Sistema interno, CRM ou Web App</option>
+                <option value="diagnostico">Chatbot com Inteligência Artificial</option>
+                <option value="manutencao">Sistema para Delivery ou Agendamento</option>
                 <option value="outro">Outro</option>
             </select>
             {error && <div className={styles.error}>{error}</div>}
